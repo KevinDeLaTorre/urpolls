e212fa0ebea2b5876ed93b424175a0cb2c4fa46c
Kevin De La Torre <kevindelatorre@live.com>
2022-03-07 19:37:46 -0600

    Add static_pages controller and set simple home page as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'static_pages#home'
+  get  '/home', to: 'static_pages#home'
+
+  devise_for :users
 end
