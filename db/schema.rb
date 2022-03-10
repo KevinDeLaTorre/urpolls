6579dcfbd35bc9d109f9ff1de3560b2f961c3a20
Kevin De La Torre <kevindelatorre@live.com>
2022-03-10 12:42:05 -0600

    Add schema, forgot to include in last commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_08_014926) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_09_025238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "poll_choices", force: :cascade do |t|
+    t.bigint "poll_id", null: false
+    t.string "content"
+    t.integer "count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["poll_id"], name: "index_poll_choices_on_poll_id"
+  end
 
   create_table "polls", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -35,5 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_08_014926) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "poll_choices", "polls"
   add_foreign_key "polls", "users"
 end
