e9d97dc8a681282fb506413f509b67c2e33b834a
Kevin De La Torre <kevindelatorre@live.com>
2022-03-10 12:59:43 -0600

    Add validation to block empty strings

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,3 +1,5 @@
 class Poll < ApplicationRecord
   belongs_to :user, dependent: :destroy
+
+  validates :title, allow_blank: false
 end
--- app/models/poll_choice.rb
+++ app/models/poll_choice.rb
@@ -1,3 +1,5 @@
 class PollChoice < ApplicationRecord
   belongs_to :poll
+
+  validates :content, allow_blank: false
 end
