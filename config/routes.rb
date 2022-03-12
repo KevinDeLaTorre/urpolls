25bd687985f1f9ebea75c729c6253244463dd361
Kevin De La Torre <kevindelatorre@live.com>
2022-03-11 18:59:05 -0600

    Add get route to destroy action because rails had issue with no get route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get  '/home', to: 'static_pages#home'
 
   devise_for :users
+  devise_scope :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :polls
 end
