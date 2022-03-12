5cecc42a260882e0063d0555a9bbf4bb9c64d9a8
Kevin De La Torre <kevindelatorre@live.com>
2022-03-11 23:07:28 -0600

    Fix allow_blank validating uniqueness as well

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,6 +1,14 @@
 class Poll < ApplicationRecord
+  before_save :process_title
+
   belongs_to :user
   has_many :poll_choices, dependent: :destroy
 
-  validates :title, uniqueness: { allow_blank: false }
+  validates :title, presence: true, allow_blank: false
+
+  private
+
+  def process_title
+    self.title = self.title.capitalize
+  end
 end
--- app/models/poll_choice.rb
+++ app/models/poll_choice.rb
@@ -1,5 +1,17 @@
 class PollChoice < ApplicationRecord
+  before_save :process_content
+
   belongs_to :poll
 
-  validates :content, uniqueness: { allow_blank: false }
+  validates :content, presence: true, allow_blank: false
+
+  def reset_count
+    self.count = 0
+  end
+
+  private
+
+  def process_content
+    self.content = self.content.capitalize
+  end
 end
