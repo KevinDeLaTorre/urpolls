67f4c7336810c8ede42cb3ad61466725a8ec40d1
Kevin De La Torre <kevindelatorre@live.com>
2022-03-11 23:10:40 -0600

    Clear some query issues

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@ class StaticPagesController < ApplicationController
 
   def home 
     if user_signed_in?
-      @polls = current_user.polls.includes([:poll_choices]).paginate(page: params[:page], per_page: DEFAULTITEMSPERPAGE)
+      @polls = current_user.polls.includes(:poll_choices, :user).paginate(page: params[:page], per_page: DEFAULTITEMSPERPAGE)
     else
-      @polls = Poll.includes([:poll_choices]).all.paginate(page: params[:page], per_page: DEFAULTITEMSPERPAGE)
+      @polls = Poll.includes(:poll_choices, :user).all.paginate(page: params[:page], per_page: DEFAULTITEMSPERPAGE)
     end
   end
 end
