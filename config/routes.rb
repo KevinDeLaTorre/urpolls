1d3824d153cef2871efb52b07c2835c8c226abf7
Kevin De La Torre <kevindelatorre@live.com>
2022-03-14 20:49:16 -0500

    Add styling to site

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/home', to: 'static_pages#home'
 
-  devise_for :users
+  devise_for :users, :path => 'accounts'
   devise_scope :user do
-    get 'users/sign_out' => 'devise/sessions#destroy'
+    get 'accounts/sign_out' => 'devise/sessions#destroy'
   end
-  resources :polls
+
+  resources :users do
+    resources :votes, only: [:create, :destroy, :index]
+    resources :polls
+  end
+  resources :polls, only: [:index, :show]
 end
