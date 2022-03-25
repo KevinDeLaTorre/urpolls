8b36da239b0265ac24e2fd9533ae6e0b43b6bb02
Kevin De La Torre <kevindelatorre@live.com>
2022-03-24 21:44:10 -0500

    Fix n+1 for users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @votes = @user.votes.includes([:poll_choice, :poll]).paginate(page: params[:page], per_page: DEFAULTITEMSPERPAGE)
   end
 end
