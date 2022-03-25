1fcedd7d5f2a41eed4ceba16c82cf39f15c7f78f
Kevin De La Torre <kevindelatorre@live.com>
2022-03-24 21:43:49 -0500

    Add basic vote functionality

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,30 @@
 class VotesController < ApplicationController
-  
+  before_action :set_user
+
   def index
+    @votes = @user.votes.includes(:poll_choice, :poll)
   end
 
   def create
+    @vote = @user.votes.build(vote_params)
+    if !@vote.save
+      flash[:danger] = "You already voted for this poll"
+      redirect_to poll_path(@vote.poll_id)
+    else
+      redirect_to user_votes_path(@user)
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def vote_params
+    params.permit(:user_id, :poll_choice_id, :poll_id)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
